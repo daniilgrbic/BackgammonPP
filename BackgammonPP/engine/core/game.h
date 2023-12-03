1dcf8168ed4869584da3303ca4a0e6bbaac83d4c
Александар Зечевић <mi20045@alas.matf.bg.ac.rs>
2023-12-03 13:32:43 +0100

    bugfix: quick fix for linker error

--- BackgammonPP/engine/core/game.h
+++ BackgammonPP/engine/core/game.h
@@ -11,13 +11,14 @@
 class Game
 {
 public:
-    Game();
+    virtual ~Game() = 0;
     virtual std::vector<Turn> generateLegalTurns() = 0;
     void playTurn(Turn turn);
     std::optional<GameResult> getResult() const;
-    virtual bool isFinished(PlayerColor player) const;
+    virtual bool isFinished(PlayerColor player) const = 0;
 
 protected:
+    Game();
     bool isBlot(int point, PlayerColor player) const;
     bool isBlocked(int point, PlayerColor player) const;
     bool isBearingOff(PlayerColor player) const;
@@ -27,6 +28,6 @@ protected:
     std::vector<Turn> m_history;
     std::optional<GameResult> m_result;
 
-private:
-    const int CHECKERS_COUNT;
+// private:
+//     const int CHECKERS_COUNT;
 };
