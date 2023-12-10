bed4dc5ccb8be6f9fa4a9288701a854310829fcd
mgrujcic <markogrujcic@hotmail.com>
2023-12-10 16:25:59 +0100

    fixing git issue

--- BackgammonPP/boardsidebar.cpp
+++ BackgammonPP/boardsidebar.cpp
@@ -1,6 +1,24 @@
-#include "sidebar.h"
-
-SideBar::SideBar()
+#include "boardsidebar.h"
+BoardSideBar::BoardSideBar(QGraphicsItem* parent, qreal width, qreal height)
+    : QGraphicsItem(parent),
+      m_width(width),
+      m_height(height)
 {
+    qreal holderWidth = m_width / 1.25;
+    qreal holderHeight = m_height * 0.33;//fix constants
+    qreal holderX = (m_width - holderWidth) / 2;
+    top = new SidebarCheckerHolder(this, holderWidth, holderHeight);
+    top->setPos(holderX, (m_height/2 - holderHeight) / 2);
+    bottom = new SidebarCheckerHolder(this, holderWidth, holderHeight);
+    bottom->setPos(holderX, m_height/2 + (m_height/2 - holderHeight) / 2);
+}
+
+QRectF BoardSideBar::boundingRect() const {
+    return QRectF(0, 0, m_width, m_height);
+}
+
+void BoardSideBar::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
+    painter->setBrush(QBrush(Qt::lightGray));
+    painter->drawRect(boundingRect());
 
 }
