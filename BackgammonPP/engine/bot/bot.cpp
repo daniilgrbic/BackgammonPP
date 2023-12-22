79c9389dd7d206eb9389b45fb364146c91ce6cd5
Igor <igorkandic2@gmail.com>
2023-12-22 19:58:14 +0100

    implemented bestTurn

--- BackgammonPP/engine/bot/bot.cpp
+++ BackgammonPP/engine/bot/bot.cpp
@@ -1,6 +1,7 @@
 #include "bot.hpp"
 #include "engine/core/game.h"
 #include "engine/core/turn.h"
+#include "engine/core/playercolor.h"
 
 namespace AI{
 
@@ -10,11 +11,36 @@ namespace AI{
     Bot::~Bot(){
         delete network;
     }
-    Turn Bot::bestTurn(const Game& game, const BoardState& state){
+    Turn Bot::bestTurn(PlayerColor color, const std::vector<Turn>& turns){
+        if(color == PlayerColor::WHITE){
+            std::pair<double, Turn> bestTurn = {network->evaluateNetwork(network->inputFromState(turns[0].m_finalBoard)), turns[0]};
+            for(const auto& turn : turns){
+                BoardState b = turn.m_finalBoard;
+                double eval = network->evaluateNetwork(network->inputFromState(b));
+                if(bestTurn.first < eval){
+                    bestTurn.first = eval;
+                    bestTurn.second = turn;
+                };
+            }
+            return bestTurn.second;
+        }else{
+            std::pair<double, Turn> bestTurn = {network->evaluateNetwork(network->inputFromState(turns[0].m_finalBoard.mirror())), turns[0]};
+            for(const auto& turn : turns){
+                BoardState b = turn.m_finalBoard;
+                double eval = network->evaluateNetwork(network->inputFromState(b.mirror()));
+                if(bestTurn.first < eval){
+                    bestTurn.first = eval;
+                    bestTurn.second = turn;
+                };
+            }
+            return bestTurn.second;
+        }
+
+
 
     }
     bool Bot::shouldDouble(const Game& game, const BoardState& state){
-
+        return true;
     }
 }
 
--- BackgammonPP/engine/bot/bot.hpp
+++ BackgammonPP/engine/bot/bot.hpp
@@ -5,13 +5,14 @@
 #include "engine/core/boardstate.h"
 #include "engine/core/game.h"
 #include "engine/core/turn.h"
+#include "engine/core/playercolor.h"
 
 namespace AI{
     class Bot{
     public:
         Bot(std::string filepath);
         ~Bot();
-        Turn bestTurn(const Game& game, const BoardState& state);
+        Turn bestTurn(PlayerColor color, const std::vector<Turn>& turns);
         bool shouldDouble(const Game& game, const BoardState& state);
     private:
         Network* network;
