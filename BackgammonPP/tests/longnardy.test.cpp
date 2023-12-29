cc88ff4ab17daf4c9a017ee32579681110990195
daniilgrbic <daniilgrbic@gmail.com>
2023-12-29 21:11:13 +0100

    Fix bearoff, finalize tests for Nardy

--- BackgammonPP/tests/longnardy.test.cpp
+++ BackgammonPP/tests/longnardy.test.cpp
@@ -10,22 +10,37 @@ public:
     void setCurrentRoll(PlayerColor onRoll, const std::vector<int>& dice) {
         m_currentRoll = {onRoll, dice};
     }
+
+    void setBoardState(BoardState board) {
+        m_board = board;
+    }
+
+    void fillHistory() {
+        // this is used to gaslight the turn generator into believing
+        // it's not playing an initial turn
+        m_history.resize(2);
+    }
 };
 
 void printTurns(const std::vector<Turn>& turns) {
     for(const auto &turn : turns) {
         for(const auto &move : turn.m_moves) {
-            std::cout << std::get<int>(move.m_from) << "->" << std::get<int>(move.m_to) << " ";
+            if(std::holds_alternative<int>(move.m_from)) std::cout << std::get<int>(move.m_from);
+            else std::cout << ((std::get<SpecialPosition>(move.m_from) == SpecialPosition::BAR) ? "BAR" : "OFF");
+            std::cout << "->";
+            if(std::holds_alternative<int>(move.m_to)) std::cout << std::get<int>(move.m_to);
+            else std::cout << ((std::get<SpecialPosition>(move.m_to) == SpecialPosition::BAR) ? "BAR" : "OFF");
+            std::cout << " ";
         }
         std::cout << std::endl;
     }
 }
 
-TEST_CASE( "Long Nardy turn generation", "[class][ruleset]" ) {
-    SECTION("Should construct a game of Long Nardy") {
+TEST_CASE("Long Nardy game construction") {
 
+    SECTION("Should construct a game of Long Nardy") {
         for(int i = 0; i < 20; i++) {
-            // Arrange
+            // Arrange & Act
             auto game = LongNardyTestHelper();
             auto onRoll = game.currentRoll().onRoll();
             auto dice = game.currentRoll().dice();
@@ -41,6 +56,9 @@ TEST_CASE( "Long Nardy turn generation", "[class][ruleset]" ) {
             }
         }
     }
+}
+
+TEST_CASE("Long Nardy first turn generation") {
 
     SECTION("Should generate legal turns for white player - first roll 2 and 3") {
         // Arrange
@@ -136,28 +154,198 @@ TEST_CASE( "Long Nardy turn generation", "[class][ruleset]" ) {
         REQUIRE(finalBoard.point(24).count() == 13);
         REQUIRE(finalBoard.point(18).count() == 2);
     }
+}
 
-    SECTION("Should mirror board for black player") {
+TEST_CASE("Long Nardy prime rule") {
+
+    SECTION("Should forbid forming a prime if no opponent checkers in front of it") {
         // Arrange
-        auto bs = BoardState();
-        bs.point(1).add(PlayerColor::WHITE, 4);
-        bs.point(3).add(PlayerColor::BLACK, 3);
-        bs.point(10).add(PlayerColor::WHITE, 1);
-        bs.point(11).add(PlayerColor::BLACK, 2);
-        bs.point(14).add(PlayerColor::WHITE, 5);
-        bs.point(19).add(PlayerColor::BLACK, 6);
+        BoardState board{};
+        board.point(24).add(PlayerColor::WHITE, 9);
+        board.point(9).add(PlayerColor::WHITE, 1);
+        board.point(8).add(PlayerColor::WHITE, 1);
+        board.point(7).add(PlayerColor::WHITE, 1);
+        board.point(5).add(PlayerColor::WHITE, 1);
+        board.point(4).add(PlayerColor::WHITE, 1);
+        board.point(10).add(PlayerColor::WHITE, 1);
+        board.point(Point::centralMirrorId(24)).add(PlayerColor::BLACK, 15);
+
+        auto game = LongNardyTestHelper();
+        game.setCurrentRoll(PlayerColor::WHITE, {4, 1});
+        game.setBoardState(board);
+        game.fillHistory();
 
         // Act
-        auto mbs = BoardState::centralMirror(bs);
-        auto mmbs = BoardState::centralMirror(mbs);
+        auto turns = game.generateLegalTurns();
 
         // Assert
-        REQUIRE(bs == mmbs);
-        REQUIRE(mbs.point(Point::centralMirrorId(1)) == Point{PlayerColor::WHITE, 4});
-        REQUIRE(mbs.point(Point::centralMirrorId(3)) == Point{PlayerColor::BLACK, 3});
-        REQUIRE(mbs.point(Point::centralMirrorId(10)) == Point{PlayerColor::WHITE, 1});
-        REQUIRE(mbs.point(Point::centralMirrorId(11)) == Point{PlayerColor::BLACK, 2});
-        REQUIRE(mbs.point(Point::centralMirrorId(14)) == Point{PlayerColor::WHITE, 5});
-        REQUIRE(mbs.point(Point::centralMirrorId(19)) == Point{PlayerColor::BLACK, 6});
+        int blocked;
+        for(const auto& turn : turns) {
+            blocked = 0;
+            for(int p = 4; p <= 9; p++) {
+                if(turn.m_finalBoard.point(p).owner().has_value())
+                    blocked++;
+            }
+            REQUIRE(blocked < 6);
+
+            blocked = 0;
+            for(int p = 5; p <= 10; p++) {
+                if(turn.m_finalBoard.point(p).owner().has_value())
+                    blocked++;
+            }
+            REQUIRE(blocked < 6);
+        }
+    }
+
+    SECTION("Should allow forming a prime if there are opponent checkers in front of it") {
+        // Arrange
+        BoardState board{};
+        board.point(24).add(PlayerColor::WHITE, 9);
+        board.point(9).add(PlayerColor::WHITE, 1);
+        board.point(8).add(PlayerColor::WHITE, 1);
+        board.point(7).add(PlayerColor::WHITE, 1);
+        board.point(5).add(PlayerColor::WHITE, 1);
+        board.point(4).add(PlayerColor::WHITE, 1);
+        board.point(10).add(PlayerColor::WHITE, 1);
+        board.point(Point::centralMirrorId(24)).add(PlayerColor::BLACK, 14);
+        board.point(Point::centralMirrorId(13)).add(PlayerColor::BLACK, 1);
+
+        auto game = LongNardyTestHelper();
+        game.setCurrentRoll(PlayerColor::WHITE, {4, 1});
+        game.setBoardState(board);
+        game.fillHistory();
+
+        // Act
+        auto turns = game.generateLegalTurns();
+
+        // Assert
+        int blocked;
+        bool primeFormed = false;
+        for(const auto& turn : turns) {
+            blocked = 0;
+            for(int p = 4; p <= 9; p++) {
+                if(turn.m_finalBoard.point(p).owner().has_value())
+                    blocked++;
+            }
+            if(blocked >= 6) {
+                primeFormed = true;
+                break;
+            }
+
+            blocked = 0;
+            for(int p = 5; p <= 10; p++) {
+                if(turn.m_finalBoard.point(p).owner().has_value())
+                    blocked++;
+            }
+            if(blocked >= 6) {
+                primeFormed = true;
+                break;
+            }
+        }
+        REQUIRE(primeFormed);
+    }
+}
+
+TEST_CASE("Long Nardy bearoff") {
+
+    SECTION("Should properly bear off - impossible to remove both checkers") {
+        // Arrange
+        BoardState board{};
+        board.point(4).add(PlayerColor::WHITE, 2);
+        board.point(6).add(PlayerColor::WHITE, 2);
+
+        auto game = LongNardyTestHelper();
+        game.setCurrentRoll(PlayerColor::WHITE, {5, 6});
+        game.setBoardState(board);
+        game.fillHistory();
+
+        // Act
+        auto turns = game.generateLegalTurns();
+
+        // Assert
+        for(const auto& turn : turns) {
+            REQUIRE(turn.m_finalBoard.point(1).count() == 1);
+            REQUIRE(turn.m_finalBoard.point(4).count() == 2);
+        }
+    }
+
+    SECTION("Should properly bear off - impossible to remove any checkers") {
+        // Arrange
+        BoardState board{};
+        board.point(4).add(PlayerColor::WHITE, 2);
+        board.point(6).add(PlayerColor::WHITE, 2);
+
+        auto game = LongNardyTestHelper();
+        game.setCurrentRoll(PlayerColor::WHITE, {4, 5});
+        game.setBoardState(board);
+
+        // Act
+        auto turns = game.generateLegalTurns();
+
+        // Assert
+        for(const auto& turn : turns) {
+            REQUIRE(turn.m_finalBoard.point(1).count() == 1);
+            REQUIRE(turn.m_finalBoard.point(2).count() == 1);
+            REQUIRE(turn.m_finalBoard.point(4).count() == 2);
+        }
+    }
+
+    SECTION("Should properly bear off - exactly remove last 2 checkers") {
+        // Arrange
+        BoardState board{};
+        board.point(2).add(PlayerColor::WHITE, 1);
+        board.point(5).add(PlayerColor::WHITE, 1);
+
+        auto game = LongNardyTestHelper();
+        game.setCurrentRoll(PlayerColor::WHITE, {2, 5});
+        game.setBoardState(board);
+        game.fillHistory();
+
+        // Act
+        auto turns = game.generateLegalTurns();
+
+        // Assert
+        for(const auto& turn : turns) {
+            REQUIRE((turn.m_finalBoard.point(2).count() == 1
+                     or turn.m_finalBoard.off(PlayerColor::WHITE) == 2));
+        }
+    }
+
+    SECTION("Should properly bear off - remove 2 checkers with higher rolls") {
+        // Arrange
+        BoardState board{};
+        board.point(2).add(PlayerColor::WHITE, 1);
+        board.point(3).add(PlayerColor::WHITE, 1);
+
+        auto game = LongNardyTestHelper();
+        game.setCurrentRoll(PlayerColor::WHITE, {4, 5});
+        game.setBoardState(board);
+        game.fillHistory();
+
+        // Act
+        auto turns = game.generateLegalTurns();
+
+        // Assert
+        for(const auto& turn : turns) {
+            REQUIRE(turn.m_finalBoard.off(PlayerColor::WHITE) == 2);
+        }
+    }
+
+    SECTION("Should properly bear off - remove 4 checkers with a double roll") {
+        // Arrange
+        BoardState board{};
+        board.point(3).add(PlayerColor::WHITE, 5);
+
+        auto game = LongNardyTestHelper();
+        game.setCurrentRoll(PlayerColor::WHITE, {3, 3, 3, 3});
+        game.setBoardState(board);
+        game.fillHistory();
+
+        // Act
+        auto turns = game.generateLegalTurns();
+
+        // Assert
+        REQUIRE(turns.front().m_finalBoard.point(3).count() == 1);
+        REQUIRE(turns.front().m_finalBoard.off(PlayerColor::WHITE) == 4);
     }
 }
