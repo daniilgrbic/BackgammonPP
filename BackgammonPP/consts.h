fd2d9de059cba62332f4306ada05d8c658bb183b
Branko Grbić <brankogrbic01@gmail.com>
2023-12-30 22:28:08 +0000

    Increase base theme volume

--- BackgammonPP/consts.h
+++ BackgammonPP/consts.h
@@ -7,7 +7,7 @@ const qint16 MAX_USERNAME_SIZE = 20;
 const qint16 MIN_NUM_GAMES = 1;
 const qint16 MAX_NUM_GAMES = 10;
 
-const qint16 BASE_THEME_VOLUME = 0;
+const qint16 BASE_THEME_VOLUME = 99;
 const qint16 MAX_VOLUME = 100;
 
 const QString THEME_SOUND_PATH = "qrc:/audio/uSnovimaProdBane.mp3";
