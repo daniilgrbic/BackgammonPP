5bcc2b7484fe33ce82ab1d88f70545e66ca594a5
Ivan Gogić <ivangogic@protonmail.com>
2023-12-30 22:29:27 +0100

    Code cleanup

--- BackgammonPP/network/client.h
+++ BackgammonPP/network/client.h
@@ -41,23 +41,3 @@ public slots:
 private:
     QTcpSocket* m_socket;
 };
-
-//     bool connectClient(QString ipAddress);
-//     void sendStateToServer(QString state);
-//     void sendOpponentToServer(QString state);
-//     void sendNameToServer(QString name);
-
-// signals:
-//     // these should be handled in Game
-//     void connectedAsHost();
-//     void connectedAsPlayer();
-//     void connectedAsSpectator();
-//     void connectedAsWaiting();
-//     void potentialOpponent(QString oppName);
-//     // these should be handled in Player
-//     void newState(QString state);
-
-
-// public slots:
-//     void readMessage();
-//     void disconnected();
